0051511c5de1dc635e2c09806489bc25c735ca73
svgiramazan <128932682+svgiramazan@users.noreply.github.com>
2024-05-03 22:12:41 +0300

    datetime
    güncel tarih saat ve günü veren ve yazılımcının belirlediği tarihle farkını bulma

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_str
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime simdi = DateTime.Now;
+            Console.WriteLine(value:simdi);//metodlarıyla ayrı ayrı ay yıl gün saat dakika saniye istenebilir.
+            Console.WriteLine(value: simdi.Month);
+            Console.WriteLine(value: simdi.DayOfWeek);
+
+            DateTime dt = new DateTime(2003, 4, 21);
+            Console.WriteLine(dt);
+            DateTime dt2 = dt.AddHours(5);//.add ekleme işlemi yapar
+            Console.WriteLine(dt2);
+            var fark = simdi - dt;
+            Console.WriteLine(fark);
+            Console.WriteLine(fark.TotalDays);
+            Console.ReadLine();
+        }
+    }
+}
